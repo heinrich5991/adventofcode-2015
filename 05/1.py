c2337a7afe65894e2ed2c01f95984c52efbe39c7
heinrich5991 <heinrich5991@gmail.com>
2015-12-07 02:46:44 +0000

    Add 05/1 attempt 1

---
+++ 05/1.py
@@ -0,0 +1,19 @@
+from collections import Counter
+def pairs(iterable):
+    iterable = iter(iterable)
+    prev = next(iterable)
+    for elem in iterable:
+        yield (prev, elem)
+        prev = elem
+
+def first_req(string):
+    c = Counter(string)
+    return sum(c[v] for v in "aeiou")
+
+def second_req(string):
+    return any(x == y for x, y in pairs(string))
+
+def third_req(string):
+    return all(p not in ["ab", "cd", "pq", "xy"] for p in pairs(string))
+
+print(sum(first_req(s) and second_req(s) and third_req(s) for s in open('input')))
