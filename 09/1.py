5190935dfe139beb146a8e98acbdda717c87c82e
heinrich5991 <heinrich5991@gmail.com>
2015-12-09 05:21:56 +0000

    Add 09/1 attempt 1

---
+++ 09/1.py
@@ -0,0 +1,15 @@
+import itertools
+def pairs(iterable):
+    iterable = iter(iterable)
+    prev = next(iterable)
+    for elem in iterable:
+        yield (prev, elem)
+        prev = elem
+
+tokens = (line.split() for line in open('input'))
+city_pairs = [(t[0], t[2], int(t[4])) for t in tokens]
+distances = {}
+distances.update({(a, b): distance for a, b, distance in city_pairs})
+distances.update({(b, a): distance for a, b, distance in city_pairs})
+cities = {a for a, b, distance in city_pairs}
+print(min(sum(distances[(x, y)] for x, y in pairs(path)) for path in itertools.permutations(cities)))
