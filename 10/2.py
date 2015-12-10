c2576bf9d44c9d4e0398cce7612850f696a7b157
heinrich5991 <heinrich5991@gmail.com>
2015-12-10 05:09:37 +0000

    Add 10/2 solution

---
+++ 10/2.py
@@ -0,0 +1,19 @@
+import functools
+def next_impl(string):
+    iter_ = iter(string)
+    cur = next(iter_)
+    count = 1
+    for c in iter_:
+        if cur != c:
+            yield "{}{}".format(count, cur)
+            cur = c
+            count = 1
+        else:
+            count += 1
+    yield "{}{}".format(count, cur)
+
+def next_(string):
+    return "".join(next_impl(string))
+
+INPUT = "1113122113"
+print(len(list(functools.reduce(lambda x, _: next_(x), range(50), INPUT))))
